81a6f0576f9b120bf86f4ac7896f0f3cfb94cbbc
husiaodevelopers <husiaodevelopers@gmail.com>
2024-06-30 20:44:36 +0800

    Refresh .gitignore rules

--- common-util/src/main/java/com/jimmydomain/common/Main.java
+++
@@ -1,7 +0,0 @@
-package com.jimmydomain.common;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- common-util/src/main/java/com/jimmydomain/common/util/CollectionUtils.java
+++ common-util/src/main/java/com/jimmydomain/common/util/CollectionUtils.java
@@ -1,6 +1,13 @@
 package com.jimmydomain.common.util;
 
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
