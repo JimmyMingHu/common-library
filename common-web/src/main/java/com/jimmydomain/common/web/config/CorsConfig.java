cf1f142082d09960c78ed7c0ed620f5a47a95c5a
husiaodevelopers <husiaodevelopers@gmail.com>
2024-06-30 22:28:23 +0800

    add common-web about CorsConfig

---
+++ common-web/src/main/java/com/jimmydomain/common/web/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.jimmydomain.common.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+/**
+ * @author Jimmy Hu
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
